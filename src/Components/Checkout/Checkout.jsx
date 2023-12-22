3682c561e886503fdea2a96f1515903564a3b024
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2023-12-22 17:30:44 +0530

    Update Checkout.jsx

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+// import { useState } from 'react'
 import Stripe from '../Stripe/Stripe'
 import './Checkout.css'
 function Checkout() {
